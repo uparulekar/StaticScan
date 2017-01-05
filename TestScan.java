f712fa9453e75a257c2046928a127221acceac29
uparulekar <ucparule@us.ibm.com>
2017-01-05 12:19:10 -0600

    Update TestScan.java

--- TestScan.java
+++ TestScan.java
@@ -4,5 +4,10 @@ public class TestScan {
 	int a = 0;
 	int dbz = 88/a;
 	System.out.println("Value of dbz: " + dbz);
-   }
+	   
+	char arr[32];
+	for( int i = 0; i < 64; i++ )
+		arr[i] = (char)i;
+   	}
+	System.out.println("Array initialized");
 }
